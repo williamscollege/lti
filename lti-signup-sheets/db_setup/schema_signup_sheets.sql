782e62fea915a20869a091e90cd64f7fb95e5f87
dwk2 <dwk2@williams.edu>
2015-05-20 14:57:13 -0400

    Added INDEXES to tables
    - Efficiency: Added INDEXES to tables
    - Refactored schema for consistency in creating INDEXES
    - SQL: refactored one query for better efficiency

--- lti-signup-sheets/db_setup/schema_signup_sheets.sql
+++ lti-signup-sheets/db_setup/schema_signup_sheets.sql
@@ -77,7 +77,13 @@ CREATE TABLE IF NOT EXISTS `terms` (
 	`name` VARCHAR(255) NULL,
 	`start_date` TIMESTAMP,
 	`end_date` TIMESTAMP,
-	`flag_delete` BIT(1) NOT NULL DEFAULT 0
+	`flag_delete` BIT(1) NOT NULL DEFAULT 0,
+	INDEX `canvas_term_id` (`canvas_term_id`),
+	INDEX `term_idstr` (`term_idstr`),
+	INDEX `name` (`name`),
+	INDEX `start_date` (`start_date`),
+	INDEX `end_date` (`end_date`),
+	INDEX `flag_delete` (`flag_delete`)
 )  ENGINE=innodb DEFAULT CHARACTER SET=utf8 COLLATE utf8_general_ci COMMENT='Sync with data sent from PS to Canvas';
 
 CREATE TABLE IF NOT EXISTS `users` (
@@ -92,7 +98,13 @@ CREATE TABLE IF NOT EXISTS `users` (
 	`updated_at` TIMESTAMP NULL,
 	`flag_is_system_admin` BIT(1) NOT NULL DEFAULT 0,
 	`flag_is_banned` BIT(1) NOT NULL DEFAULT 0,
-	`flag_delete` BIT(1) NOT NULL DEFAULT 0
+	`flag_delete` BIT(1) NOT NULL DEFAULT 0,
+	INDEX `canvas_user_id` (`canvas_user_id`),
+	INDEX `sis_user_id` (`sis_user_id`),
+	INDEX `username` (`username`),
+	INDEX `flag_is_system_admin` (`flag_is_system_admin`),
+	INDEX `flag_is_banned` (`flag_is_banned`),
+	INDEX `flag_delete` (`flag_delete`)
 )  ENGINE=innodb DEFAULT CHARACTER SET=utf8 COLLATE utf8_general_ci COMMENT='Sync with data sent from PS to Canvas';
 /* field 'username' corresponds to Canvas LMS field called 'login_id' */
 
@@ -106,7 +118,11 @@ CREATE TABLE IF NOT EXISTS `courses` (
 	`canvas_course_id` INT NOT NULL DEFAULT 0,
 	`begins_at_str` VARCHAR(24) NULL,
 	`ends_at_str` VARCHAR(24) NULL,
-	`flag_delete` BIT(1) NOT NULL DEFAULT 0
+	`flag_delete` BIT(1) NOT NULL DEFAULT 0,
+	INDEX `course_idstr` (`course_idstr`),
+	INDEX `term_idstr` (`term_idstr`),
+	INDEX `canvas_course_id` (`canvas_course_id`),
+	INDEX `flag_delete` (`flag_delete`)
 )  ENGINE=innodb DEFAULT CHARACTER SET=utf8 COLLATE utf8_general_ci COMMENT='Sync with data sent from PS to Canvas';
 
 CREATE TABLE IF NOT EXISTS `enrollments` (
@@ -118,7 +134,14 @@ CREATE TABLE IF NOT EXISTS `enrollments` (
 	`user_id` INT NOT NULL,
 	`course_role_name` VARCHAR(48) NOT NULL,
 	`section_idstr` VARCHAR(255) NULL,
-	`flag_delete` BIT(1) NOT NULL DEFAULT 0
+	`flag_delete` BIT(1) NOT NULL DEFAULT 0,
+	INDEX `canvas_user_id` (`canvas_user_id`),
+	INDEX `canvas_course_id` (`canvas_course_id`),
+	INDEX `canvas_role_name` (`canvas_role_name`),
+	INDEX `course_idstr` (`course_idstr`),
+	INDEX `course_role_name` (`course_role_name`),
+	INDEX `section_idstr` (`section_idstr`),
+	INDEX `flag_delete` (`flag_delete`)
 )  ENGINE=innodb DEFAULT CHARACTER SET=utf8 COLLATE utf8_general_ci COMMENT='Sync with data sent from PS to Canvas';
 
 CREATE TABLE IF NOT EXISTS `course_roles` (
@@ -130,7 +153,7 @@ CREATE TABLE IF NOT EXISTS `course_roles` (
 /* priority: Highest teacher role is priority = 10; lowest alumni priority is > 30 */
 
 CREATE TABLE IF NOT EXISTS `sus_sheetgroups` (
-	`sheetgroup_id` bigint(10) unsigned NOT NULL auto_increment,
+	`sheetgroup_id` BIGINT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`updated_at` TIMESTAMP NULL,
 	`flag_delete` tinyint(1) unsigned default NULL,
@@ -140,15 +163,14 @@ CREATE TABLE IF NOT EXISTS `sus_sheetgroups` (
 	`description` text,
 	`max_g_total_user_signups` smallint signed default -1,
 	`max_g_pending_user_signups` smallint signed default -1,
-	PRIMARY KEY (`sheetgroup_id`),
-	KEY `flag_delete` (`flag_delete`),
-	KEY `owner_user_id` (`owner_user_id`),
-	KEY `flag_is_default` (`flag_is_default`),
-	KEY `name` (`name`)
+	INDEX `flag_delete` (`flag_delete`),
+	INDEX `owner_user_id` (`owner_user_id`),
+	INDEX `flag_is_default` (`flag_is_default`),
+	INDEX `name` (`name`)
 )  ENGINE=innodb DEFAULT CHARACTER SET=utf8 COLLATE utf8_general_ci COMMENT='For managing collections of related sheets';
 
 CREATE TABLE IF NOT EXISTS `sus_sheets` (
-	`sheet_id` bigint(10) unsigned NOT NULL auto_increment,
+	`sheet_id` BIGINT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`updated_at` TIMESTAMP NULL,
 	`flag_delete` tinyint(1) unsigned default NULL,
@@ -168,25 +190,24 @@ CREATE TABLE IF NOT EXISTS `sus_sheets` (
 	`flag_alert_admin_signup` tinyint(1) unsigned default NULL,
 	`flag_alert_admin_imminent` tinyint(1) unsigned default NULL,
 	`flag_private_signups` int(1) default '1',
-	PRIMARY KEY (`sheet_id`),
-	KEY `flag_delete` (`flag_delete`),
-	KEY `owner_user_id` (`owner_user_id`),
-	KEY `sheetgroup_id` (`sheetgroup_id`),
-	KEY `name` (`name`),
-	KEY `type` (`type`),
-	KEY `begin_date` (`begin_date`),
-	KEY `end_date` (`end_date`),
-	KEY `flag_alert_owner_change` (`flag_alert_owner_change`),
-	KEY `flag_alert_owner_signup` (`flag_alert_owner_signup`),
-	KEY `flag_alert_owner_imminent` (`flag_alert_owner_imminent`),
-	KEY `flag_alert_admin_change` (`flag_alert_admin_change`),
-	KEY `flag_alert_admin_signup` (`flag_alert_admin_signup`),
-	KEY `flag_alert_admin_imminent` (`flag_alert_admin_imminent`),
-	KEY `flag_private_signups` (`flag_private_signups`)
+	INDEX `flag_delete` (`flag_delete`),
+	INDEX `owner_user_id` (`owner_user_id`),
+	INDEX `sheetgroup_id` (`sheetgroup_id`),
+	INDEX `name` (`name`),
+	INDEX `type` (`type`),
+	INDEX `begin_date` (`begin_date`),
+	INDEX `end_date` (`end_date`),
+	INDEX `flag_alert_owner_change` (`flag_alert_owner_change`),
+	INDEX `flag_alert_owner_signup` (`flag_alert_owner_signup`),
+	INDEX `flag_alert_owner_imminent` (`flag_alert_owner_imminent`),
+	INDEX `flag_alert_admin_change` (`flag_alert_admin_change`),
+	INDEX `flag_alert_admin_signup` (`flag_alert_admin_signup`),
+	INDEX `flag_alert_admin_imminent` (`flag_alert_admin_imminent`),
+	INDEX `flag_private_signups` (`flag_private_signups`)
 )  ENGINE=innodb DEFAULT CHARACTER SET=utf8 COLLATE utf8_general_ci COMMENT='Contains the high-level sheet data (name, descr, etc.)';
 
 CREATE TABLE IF NOT EXISTS `sus_openings` (
-	`opening_id` bigint(10) unsigned NOT NULL auto_increment,
+	`opening_id` BIGINT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`updated_at` TIMESTAMP NULL,
 	`flag_delete` tinyint(1) unsigned default NULL,
@@ -199,32 +220,30 @@ CREATE TABLE IF NOT EXISTS `sus_openings` (
 	`end_datetime` TIMESTAMP NULL,
 	`location` varchar(255) default NULL,
 	`admin_comment` varchar(255) default NULL,
-	PRIMARY KEY (`opening_id`),
-	KEY `flag_delete` (`flag_delete`),
-	KEY `sheet_id` (`sheet_id`),
-	KEY `opening_group_id` (`opening_group_id`),
-	KEY `begin_datetime` (`begin_datetime`),
-	KEY `end_datetime` (`end_datetime`),
-	KEY `location` (`location`),
-	KEY `name` (`name`)
+	INDEX `flag_delete` (`flag_delete`),
+	INDEX `sheet_id` (`sheet_id`),
+	INDEX `opening_group_id` (`opening_group_id`),
+	INDEX `name` (`name`),
+	INDEX `begin_datetime` (`begin_datetime`),
+	INDEX `end_datetime` (`end_datetime`),
+	INDEX `location` (`location`)
 )  ENGINE=innodb DEFAULT CHARACTER SET=utf8 COLLATE utf8_general_ci COMMENT='Places users can sign up - a single sheet may have multiple ';
 
 CREATE TABLE IF NOT EXISTS `sus_signups` (
-	`signup_id` bigint(10) unsigned NOT NULL auto_increment,
+	`signup_id` BIGINT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`updated_at` TIMESTAMP NULL,
 	`flag_delete` tinyint(1) unsigned default NULL,
 	`opening_id` bigint(10) unsigned default NULL,
 	`signup_user_id` bigint(10) unsigned default NULL,
 	`admin_comment` varchar(255) default NULL,
-	PRIMARY KEY (`signup_id`),
-	KEY `flag_delete` (`flag_delete`),
-	KEY `opening_id` (`opening_id`),
-	KEY `signup_user_id` (`signup_user_id`)
+	INDEX `flag_delete` (`flag_delete`),
+	INDEX `opening_id` (`opening_id`),
+	INDEX `signup_user_id` (`signup_user_id`)
 )  ENGINE=innodb DEFAULT CHARACTER SET=utf8 COLLATE utf8_general_ci COMMENT='Users signing up for openings - analogous to a list of times and dates on a piece of paper that is passed around or posted on a door and on which people would put their name';
 
 CREATE TABLE IF NOT EXISTS `sus_access` (
-	`access_id` bigint(10) unsigned NOT NULL auto_increment,
+	`access_id` BIGINT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`updated_at` TIMESTAMP NULL,
 	`sheet_id` bigint(10) unsigned default NULL,
@@ -232,12 +251,11 @@ CREATE TABLE IF NOT EXISTS `sus_access` (
 	`constraint_id` bigint(10) unsigned default NULL,
 	`constraint_data` varchar(32) default NULL,
 	`broadness` int(11) default NULL,
-	PRIMARY KEY (`access_id`),
-	KEY `sheet_id` (`sheet_id`),
-	KEY `type` (`type`),
-	KEY `constraint_id` (`constraint_id`),
-	KEY `constraint_data` (`constraint_data`),
-	KEY `broadness` (`broadness`)
+	INDEX `sheet_id` (`sheet_id`),
+	INDEX `type` (`type`),
+	INDEX `constraint_id` (`constraint_id`),
+	INDEX `constraint_data` (`constraint_data`),
+	INDEX `broadness` (`broadness`)
 )  ENGINE=innodb DEFAULT CHARACTER SET=utf8 COLLATE utf8_general_ci COMMENT='which users can signup on which sheets';
 
 # notes: a msg to 10 people will create 10 separate queued messages (to enable 1 user to delete their signup w/o effecting others)
@@ -255,7 +273,10 @@ CREATE TABLE IF NOT EXISTS `queued_messages` (
 	`action_datetime` DATETIME NULL,
 	`action_status` VARCHAR(16) NULL, /* SUCCESS|FAILURE */
 	`action_notes` TEXT NULL, /* any more detailed messages/notes about the action */
-	`flag_delete` BIT(1) NOT NULL DEFAULT 0
+	`flag_delete` BIT(1) NOT NULL DEFAULT 0,
+	INDEX `user_id` (`user_id`),
+	INDEX `sheet_id` (`sheet_id`),
+	INDEX `opening_id` (`opening_id`)
 )  ENGINE=innodb DEFAULT CHARACTER SET=utf8 COLLATE utf8_general_ci COMMENT='holds messages in queue prior to independent sending mechanism';
 
 
